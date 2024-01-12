9cc1934a4f0992e3e8febdf38e8ae164a997e1b5
Massa Timothée <timothee.massa.perso@gmail.com>
2024-01-12 17:41:06 +0100

    map dice to display Die component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
 import '../style.css'
 import Die from './Die.jsx'
 
 export default function App() {
+    const [dice, setDice] = useState(allNewDice())
+
+
+    function allNewDice() {
+        const newDice = []
+        for (let i = 0; i < 10; i++) {
+            newDice.push(Math.ceil(Math.random() * 6))
+        }
+        return newDice
+    }
+
+    const diceElement = dice.map(die => <Die value={die}/>)
     
     return (
         <main>
             <div className='dice--container'>
-                <Die value={1}/>
-                <Die value={2}/>
-                <Die value={4}/>
-                <Die value={5}/>
-                <Die value={3}/>
-                <Die value={2}/>
-                <Die value={3}/>
-                <Die value={6}/>
-                <Die value={5}/>
-                <Die value={1}/>
+                {diceElement}
             </div>
         </main>
     )
