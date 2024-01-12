5bc44b3db42e6dc89e28fe6c33c6078969ed71cb
Massa Timothée <timothee.massa.perso@gmail.com>
2024-01-12 17:27:36 +0100

    Dice component implemented and stylized

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import '../style.css'
+import Die from './Die.jsx'
 
 export default function App() {
+    
     return (
         <main>
-
+            <div className='dice--container'>
+                <Die value={1}/>
+                <Die value={2}/>
+                <Die value={4}/>
+                <Die value={5}/>
+                <Die value={3}/>
+                <Die value={2}/>
+                <Die value={3}/>
+                <Die value={6}/>
+                <Die value={5}/>
+                <Die value={1}/>
+            </div>
         </main>
     )
 }
