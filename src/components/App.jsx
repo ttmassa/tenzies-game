d1f8c28ebd903405170104ba75a4d8c447afa939
Massa Timothée <timothee.massa.perso@gmail.com>
2024-01-15 13:04:22 +0100

    save best time to localStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,9 +8,8 @@ import Confetti from 'react-confetti'
 /*
 To-do:
 - Replace numbers by dots (done)
-- Display best score (localStorage)
-- Display time to beat
-
+- Display best score (localStorage) (done)
+- Display time to beat (done)
 */
 
 export default function App() {
@@ -18,7 +17,9 @@ export default function App() {
     const [tenzies, setTenzies] = useState(false)
     const [game, setGame] = useState(false)
     const [time, setTime] = useState(0)
+    const [bestTime, setBestTime] = useState(JSON.parse(localStorage.getItem('bestTime')) || 0)
 
+    // Check if the game is done
     useEffect(() => {
         if (dice.every(die => die.isHeld) && dice.every(die => die.value === dice[0].value)) {
             setTenzies(true)
@@ -26,7 +27,7 @@ export default function App() {
         }
     }, [dice])
 
-    //update the timer 
+    // Update the timer 
     useEffect(() => {
         if (game) {
             const intervalId = setInterval(() => {
@@ -35,7 +36,15 @@ export default function App() {
         
             return () => clearInterval(intervalId);
         }
-      }, [game]);
+    }, [game]);
+
+    // Save time to localStorage
+    useEffect(() => {
+        if (tenzies && (time < JSON.parse(localStorage.getItem('bestTime')) || bestTime === 0)) {
+            localStorage.setItem('bestTime', JSON.stringify(time))
+            setBestTime(time)
+        }
+    }, [tenzies, bestTime, time])
 
 
     function allNewDice() {
@@ -90,7 +99,7 @@ export default function App() {
             <h1 className="title">Tenzies</h1>
             <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
             <div className='record'>
-                <h4>Best score: </h4>
+                <h4>Best score: <span className='bestTime'>{bestTime}</span>s</h4>
                 <h4 className='timer'>Current Time: <span className='time'>{time}</span>s</h4>
             </div>
             <div className='dice--container'>
